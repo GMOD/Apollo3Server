f3f5df14252758301beb5919ec88fce0e2fb4dac
Nathan Dunn <ndunn@me.com>
2020-08-26 09:55:25 -0700

    attempting travis script

--- src/integration-test/groovy/resources/travis/run-travis.sh
+++ src/integration-test/groovy/resources/travis/run-travis.sh
@@ -10,19 +10,20 @@ if [[ $TEST_SUITE == "apollo" ]]; then
 fi
 if [[ $TEST_SUITE == "python-apollo" ]]; then
   set -ex
-  cp src/integration-test/groovy/resources/travis/python-apollo.travis apollo-config.groovy
+#  cp src/integration-test/groovy/resources/travis/python-apollo.travis apollo-config.groovy
   ./grailsw run-app &
-  git clone --single-branch --branch 4.2.2 --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
+  git clone --single-branch --branch fix-all-exons --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
   cd python-apollo
-  sed -i 's|8888|8080/apollo|' `pwd`/test-data/local-arrow.yml
-  export ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/local-arrow.yml
+  sed -i 's|8888|8080/apollo|' `pwd`/test-data/local-apollo3-arrow.yml
+  ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/local-arrow.yml
+  export ARROW_GLOBAL_CONFIG_PATH
   python3 --version
   python3 -m venv .venv
   . .venv/bin/activate
   python3 --version
   pip3 install .
   pip3 install nose
-  ./bootstrap_apollo.sh --nodocker
+  ./bootstrap_apollo.sh --nodocker3
   python3 setup.py nosetests
   killall java || true
 fi
