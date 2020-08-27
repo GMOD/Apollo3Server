9ee3e1c30b15ee687ce29534ece3daeae8ac0ecc
Nathan Dunn <nathandunn@lbl.gov>
2020-08-26 19:54:29 -0700

    Fix 21 (#32)
    * fixes #21
    
    * removed explicit python3
    
    * trying python 3 again
    
    * updated python3 stuff
    
    * trying again
    
    * setting to 3.5
    
    * hopefully pip is working better now
    
    * seeing if python3 is present
    
    * updated python3 venv
    
    * adding packages directly
    
    * added 3.8
    
    * added 3.8
    
    * removed update call
    
    * pushed it through to 3.7
    
    * using the docker version

--- src/integration-test/groovy/resources/travis/run-travis.sh
+++ src/integration-test/groovy/resources/travis/run-travis.sh
@@ -15,16 +15,19 @@ if [[ $TEST_SUITE == "python-apollo" ]]; then
   ./grailsw run-app &
   git clone --single-branch --branch fix-all-exons --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
   cd python-apollo
-  sed -i 's|8888|8080/apollo|' `pwd`/test-data/local-apollo3-arrow.yml
-  ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/local-arrow.yml
+#  sed -i 's|8888|8080/apollo|' `pwd`/test-data/local-apollo3-arrow.yml
+#  ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/local-apollo3-arrow.yml
+  ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/docker-apollo3-arrow.yml
   export ARROW_GLOBAL_CONFIG_PATH
   python3 --version
   python3 -m venv .venv
+#  apt-get update
+#  apt-get install python3-venv
   . .venv/bin/activate
   python3 --version
   pip3 install .
   pip3 install nose
-  ./bootstrap_apollo.sh --nodocker3
+  ./bootstrap_apollo.sh --docker3
   python3 setup.py nosetests
   killall java || true
 fi
