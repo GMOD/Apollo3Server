c09a59b6fc396be305014a0e247d22ae003761a5
Nathan Dunn <ndunn@me.com>
2020-08-28 18:07:36 -0700

    ading neo4j locally more

--- src/integration-test/groovy/resources/travis/run-travis.sh
+++ src/integration-test/groovy/resources/travis/run-travis.sh
@@ -12,12 +12,15 @@ fi
 if [[ $TEST_SUITE == "python-apollo" ]]; then
   set -ex
 #  cp src/integration-test/groovy/resources/travis/python-apollo.travis apollo-config.groovy
+  ps -ef | grep neo4j
+#  service neo4j start
   ./grailsw run-app &
+  ps -ef | grep neo4j
   git clone --single-branch --branch fix-all-exons --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
   cd python-apollo
 #  sed -i 's|8888|8080/apollo|' `pwd`/test-data/local-apollo3-arrow.yml
-#  ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/local-apollo3-arrow.yml
-  ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/docker-apollo3-arrow.yml
+  ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/local-apollo3-arrow.yml
+#  ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/docker-apollo3-arrow.yml
   export ARROW_GLOBAL_CONFIG_PATH
   python3 --version
   python3 -m venv .venv
@@ -27,8 +30,8 @@ if [[ $TEST_SUITE == "python-apollo" ]]; then
   python3 --version
   pip3 install nose
   pip3 install .
-#  ./bootstrap_apollo.sh --local-docker3
-  ./bootstrap_apollo.sh --docker3
+  ./bootstrap_apollo.sh --local-docker3
+#  ./bootstrap_apollo.sh --docker3
   python3 setup.py nosetests
   killall java || true
 fi
