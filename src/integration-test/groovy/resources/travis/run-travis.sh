d0c76e6c6a1e260031e593f813b0470f8b63a642
Nathan Dunn <ndunn@me.com>
2020-08-29 12:29:56 -0700

    have to start grails in the right directoyr, but we confirm password first

--- src/integration-test/groovy/resources/travis/run-travis.sh
+++ src/integration-test/groovy/resources/travis/run-travis.sh
@@ -10,11 +10,18 @@ if [[ $TEST_SUITE == "apollo" ]]; then
   ./grailsw test-app -unit  || EXIT_STATUS=$? #
 fi
 if [[ $TEST_SUITE == "python-apollo" ]]; then
+  echo "Runing python-apollo tests"
   set -ex
 #  cp src/integration-test/groovy/resources/travis/python-apollo.travis apollo-config.groovy
   ps -ef | grep neo4j
+  echo "setting password 1"
+  curl -H "Content-Type: application/json" -X POST -d '{"password":"testpass"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
+  echo "setting password 2"
+  curl -v -X POST http://neo4j:neo4j@localhost:7474/user/neo4j/password -d"password=testpass"
+  echo "setting password 3 - > starting grails"
 #  service neo4j start
 #  ps -ef | grep neo4j
+  ./grailsw run-app &
   git clone --single-branch --branch fix-all-exons --depth=1 https://github.com/galaxy-genome-annotation/python-apollo
   cd python-apollo
 #  sed -i 's|8888|8080/apollo|' `pwd`/test-data/local-apollo3-arrow.yml
@@ -29,12 +36,6 @@ if [[ $TEST_SUITE == "python-apollo" ]]; then
   python3 --version
   pip3 install nose
   pip3 install .
-  echo "setting password 1"
-  curl -H "Content-Type: application/json" -X POST -d '{"password":"testpass"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password
-  echo "setting password 2"
-  curl -v -X POST http://neo4j:neo4j@localhost:7474/user/neo4j/password -d"password=testpass"
-  echo "setting password 3 - > starting grails"
-  ./grailsw run-app &
   echo "doing bootstrap"
   ./bootstrap_apollo.sh --local3
 #  ./bootstrap_apollo.sh --docker3
